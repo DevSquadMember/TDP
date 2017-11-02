66bf667c5ac7ce4836f27ad799c36a8d6a658b50
Alexis DUFRENNE <prcd2017-dufrenne@mistral01.formation.cluster>
2017-11-02 18:16:30 +0100

    Matrix product by blocks

--- TP1/dgemm.c
+++ TP1/dgemm.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "cblas.h"
 
-/**
+#define BLOC_SIZE 100
+
+/*
  *  * CBLAS_ORDER Order : ordre par ligne ou colonne
  *   * CBLAS_TRANSPOSE TransA (char) : N/n pour normal - T/t - C/c pour la transposée
  *    * CBLAS_TRANSPOSE TransB (char) : N/n pour norlal - T/t - C/c pour la transposée
@@ -19,50 +22,73 @@
  *               * ldc : décalage entre chaque ligne de C
  *                *
  *                 * TransA :
- *                  * - CblasNoTrans = (i, j, k)
- *                   * - CblasTrans = (k, i, j)
- *                    * - CblasConjTrans = (j, i, k)
- *                     */
-void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K, const double alpha, const double *A, const int lda, const double *B, const int ldb, const double beta, double *C, const int ldc) {
+ *                  * - CblasNoTrans = (i, j, k) --> (5 * size + 5) * size * size
+ *                   * - CblasTrans = (k, i, j) --> size * size * (1 + 6 * size)
+ *                    size * [(size - 1) * (6 * size + 2) + (5 * size + 2)]
+ *                     * - CblasConjTrans = (j, i, k) --> ((5 * size + 1)*size + 2) * size
+ *                      */
+void cblas_dgemm_scalar(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K, const double alpha, const double *A, const int lda, const double *B, const int ldb, const double beta, double *C, const int ldc) {
 	int n, m, k, val, b_pos, a_pos, c_pos;
-    if (TransA == CblasNoTrans) {
-    	if (lda == 1) { 
+	if (TransA == CblasNoTrans) {
+		if (lda == 1) { 
 			for (m = 0 ; m < M ; m++) {
 				for (n = 0 ; n < N ; n++) {
-					val = 0;
+					c_pos = n*ldc + m;
+					val = C[c_pos];
 					b_pos = n*ldb;
 					for (k = 0 ; k < K ; k++) {
 						val += A[k + m] * B[b_pos + k];
 					} 
-					C[n*ldc + m] = val;
+					C[c_pos] = alpha*val+beta;
 				}
 			}
 		} else {
 			for (m = 0 ; m < M ; m++) {
 				for (n = 0 ; n < N ; n++) {
-					val = 0;
+					c_pos = n*ldc + m;
+					val = C[c_pos];
 					b_pos = n*ldb;
 					for (k = 0 ; k < K ; k++) {
 						val += A[k*lda + m] * B[b_pos + k];
 					} 
-					C[n*ldc + m] = val;
+					C[c_pos] = alpha*val+beta;
 				}
 			}
 		}
 	} else if (TransA == CblasTrans) {
-		for (k = 0 ; k < K ; k++) {
-			if (k == 0) {
-				for (m = 0 ; m < M ; m++) {
-					a_pos = k*lda + m;
-					for (n = 0 ; n < N ; n++) {
-						C[n*ldc + m] = A[a_pos] * B[n*ldb + k];
-					} 
-				}
-			} else {
+		if (alpha == 1 && beta == 0) {
+			for (k = 0 ; k < K ; k++) {
 				for (m = 0 ; m < M ; m++) {
 					a_pos = k*lda + m;
 					for (n = 0 ; n < N ; n++) {
 						C[n*ldc + m] += A[a_pos] * B[n*ldb + k];
+					} 
+				}
+			}
+		} else if (alpha == 1) {
+			for (k = 0 ; k < K ; k++) {
+				for (m = 0 ; m < M ; m++) {
+					a_pos = k*lda + m;
+					for (n = 0 ; n < N ; n++) {
+						C[n*ldc + m] += A[a_pos] * B[n*ldb + k] + beta;
+					} 
+				}
+			}
+		} else if (beta == 0) {
+			for (k = 0 ; k < K ; k++) {
+				for (m = 0 ; m < M ; m++) {
+					a_pos = k*lda + m;
+					for (n = 0 ; n < N ; n++) {
+						C[n*ldc + m] += alpha*A[a_pos] * B[n*ldb + k];
+					} 
+				}
+			}
+		} else {
+			for (k = 0 ; k < K ; k++) {
+				for (m = 0 ; m < M ; m++) {
+					a_pos = k*lda + m;
+					for (n = 0 ; n < N ; n++) {
+						C[n*ldc + m] += alpha*(A[a_pos] * B[n*ldb + k]) + beta;
 					} 
 				}
 			}
@@ -71,28 +97,124 @@ void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA
 		if (lda == 1) { 
 			for (n = 0 ; n < N ; n++) {
 				b_pos = n*ldb;
-				c_pos = n*ldc;
+				a_pos = n*ldc;
 				for (m = 0 ; m < M ; m++) {
-					val = 0;
+					c_pos = a_pos + m;
+					val = C[c_pos];
 					for (k = 0 ; k < K ; k++) {
-						val += A[k + m] * B[b_pos + k];
+						val += alpha * A[k + m] * B[b_pos + k] + beta;
 					} 
-					C[c_pos + m] = val;
+					C[c_pos] = val;
 				}
 			}
 		} else {
 			for (n = 0 ; n < N ; n++) {
 				b_pos = n*ldb;
-				c_pos = n*ldc;
+				a_pos = n*ldc;
 				for (m = 0 ; m < M ; m++) {
-					val = 0;
+					c_pos = a_pos + m;
+					val = C[c_pos];
 					for (k = 0 ; k < K ; k++) {
-						val += A[k*lda + m] * B[b_pos + k];
+						val += alpha * A[k*lda + m] * B[b_pos + k] + beta;
 					} 
-					C[c_pos + m] = val;
+					C[c_pos] = val;
 				}
 			}
 		}
+	}
+}
+
+void cblas_dgemm_scalar_opti(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K, const double alpha, const double *A, const int lda, const double *B, const int ldb, const double beta, double *C, const int ldc) {
+	int n, m, k, val, b_pos, a_pos, c_pos;
+	if (alpha == 1 && beta == 0) {
+		for (k = 0 ; k < K ; k++) {
+			for (m = 0 ; m < M ; m++) {
+				a_pos = k*lda + m;
+				for (n = 0 ; n < N ; n++) {
+					C[n*ldc + m] += A[a_pos] * B[n*ldb + k];
+				} 
+			}
+		}
+	} else if (alpha == 1) {
+		for (k = 0 ; k < K ; k++) {
+			for (m = 0 ; m < M ; m++) {
+				a_pos = k*lda + m;
+				for (n = 0 ; n < N ; n++) {
+					C[n*ldc + m] += A[a_pos] * B[n*ldb + k] + beta;
+				} 
+			}
+		}
+	} else if (beta == 0) {
+		for (k = 0 ; k < K ; k++) {
+			for (m = 0 ; m < M ; m++) {
+				a_pos = k*lda + m;
+				for (n = 0 ; n < N ; n++) {
+					C[n*ldc + m] += alpha*A[a_pos] * B[n*ldb + k];
+				} 
+			}
+		}
+	} else {
+		for (k = 0 ; k < K ; k++) {
+			for (m = 0 ; m < M ; m++) {
+				a_pos = k*lda + m;
+				for (n = 0 ; n < N ; n++) {
+					C[n*ldc + m] += alpha*(A[a_pos] * B[n*ldb + k]) + beta;
+				} 
+			}
+		}
+	}
+}
+
+void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K, const double alpha, const double *A, const int lda, const double *B, const int ldb, const double beta, double *C, const int ldc) {
+	if (M <= BLOC_SIZE && N <= BLOC_SIZE && K <= BLOC_SIZE) {
+		cblas_dgemm_scalar_opti(Order, TransA, TransB, M, N, K, alpha, A, lda, B, ldb, beta, C, ldc);
+		return;
+	}
+
+	int i, j, k;
+	int i_pos, j_pos, k_pos;
+	int nb_m_blocks, nb_k_blocks, nb_n_blocks;
+	int m_size, k_size, n_size;
+	int last_m_size, last_k_size, last_n_size;
+	int a_index, b_index, c_index;
+	nb_m_blocks = ceil(M/BLOC_SIZE);
+	nb_k_blocks = ceil(K/BLOC_SIZE);
+	nb_n_blocks = ceil(N/BLOC_SIZE);
+	last_m_size = M - BLOC_SIZE*(nb_m_blocks-1);
+	last_k_size = K - BLOC_SIZE*(nb_k_blocks-1);
+	last_n_size = N - BLOC_SIZE*(nb_n_blocks-1);
+
+	/*double* A_block[nb_m_blocks*nb_k_blocks];
+ * 	double* B_block[nb_k_blocks*nb_n_blocks];
+ * 		double* C_block[nb_m_blocks*nb_n_blocks];*/
+
+	for (j = 0 ; j < nb_n_blocks ; j++) {
+		if (j < nb_n_blocks - 1) {
+			n_size = BLOC_SIZE;
+		} else {
+			n_size = last_n_size;
+		}
+		j_pos = j*BLOC_SIZE;
+		for (i = 0 ; i < nb_m_blocks ; i++) {
+			if (i < nb_m_blocks - 1) {
+				m_size = BLOC_SIZE;
+			} else {
+				m_size = last_m_size;
+			}
+			i_pos = i*BLOC_SIZE;
+			c_index = i_pos + j_pos*M;
+			for (k = 0 ; k < nb_k_blocks ; k++) {
+				k_pos = k*BLOC_SIZE;
+				a_index = i_pos + k_pos*M;
+				b_index = k_pos + j_pos*K;
+				if (k < nb_k_blocks - 1) {
+					k_size = BLOC_SIZE;
+				} else {
+					k_size = last_k_size;
+				}
+				cblas_dgemm_scalar_opti(Order, TransA, TransB, m_size, n_size, k_size, alpha, &(A[a_index]), lda, &(B[b_index]), ldb, beta, &(C[c_index]), ldc);
+			}
+		} 
 	}
 }
 
--- TP1/driver.c
+++ TP1/driver.c
@@ -1,15 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "cblas.h"
 #include "util.h"
 #include "perf.h"
 
-#define MIN 8
-#define MAX 220
+#define MIN 50
+#define MAX 2000
 #define INNER 100
-#define COEF 1
-#define ADD 1
-//1.25
+#define COEF 1.25
+#define ADD 0
+
+void cblas_dgemm_scalaire(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K, const double alpha, const double *A, const int lda, const double *B, const int ldb, const double beta, double *C, const int ldc) {
+  int n, m, k, val, b_pos, a_pos, c_pos;
+  if (TransA == CblasNoTrans) {
+    if (lda == 1) { 
+      for (m = 0 ; m < M ; m++) {
+        for (n = 0 ; n < N ; n++) {
+          c_pos = n*ldc + m;
+          val = C[c_pos];
+          b_pos = n*ldb;
+          for (k = 0 ; k < K ; k++) {
+            val += A[k + m] * B[b_pos + k];
+          } 
+          C[c_pos] = alpha*val+beta;
+        }
+      }
+    } else {
+      for (m = 0 ; m < M ; m++) {
+        for (n = 0 ; n < N ; n++) {
+          c_pos = n*ldc + m;
+          val = C[c_pos];
+          b_pos = n*ldb;
+          for (k = 0 ; k < K ; k++) {
+            val += A[k*lda + m] * B[b_pos + k];
+          } 
+          C[c_pos] = alpha*val+beta;
+        }
+      }
+    }
+  } else if (TransA == CblasTrans) {
+    if (alpha == 1 && beta == 0) {
+      for (k = 0 ; k < K ; k++) {
+        for (m = 0 ; m < M ; m++) {
+          a_pos = k*lda + m;
+          for (n = 0 ; n < N ; n++) {
+            C[n*ldc + m] += A[a_pos] * B[n*ldb + k];
+          } 
+        }
+      }
+    } else if (alpha == 1) {
+      for (k = 0 ; k < K ; k++) {
+        for (m = 0 ; m < M ; m++) {
+          a_pos = k*lda + m;
+          for (n = 0 ; n < N ; n++) {
+            C[n*ldc + m] += A[a_pos] * B[n*ldb + k] + beta;
+          } 
+        }
+      }
+    } else if (beta == 0) {
+      for (k = 0 ; k < K ; k++) {
+        for (m = 0 ; m < M ; m++) {
+          a_pos = k*lda + m;
+          for (n = 0 ; n < N ; n++) {
+            C[n*ldc + m] += alpha*A[a_pos] * B[n*ldb + k];
+          } 
+        }
+      }
+    } else {
+      for (k = 0 ; k < K ; k++) {
+        for (m = 0 ; m < M ; m++) {
+          a_pos = k*lda + m;
+          for (n = 0 ; n < N ; n++) {
+            C[n*ldc + m] += alpha*(A[a_pos] * B[n*ldb + k]) + beta;
+          } 
+        }
+      }
+    }
+  } else {
+    if (lda == 1) { 
+      for (n = 0 ; n < N ; n++) {
+        b_pos = n*ldb;
+        a_pos = n*ldc;
+        for (m = 0 ; m < M ; m++) {
+          c_pos = a_pos + m;
+          val = C[c_pos];
+          for (k = 0 ; k < K ; k++) {
+            val += alpha * A[k + m] * B[b_pos + k] + beta;
+          } 
+          C[c_pos] = val;
+        }
+      }
+    } else {
+      for (n = 0 ; n < N ; n++) {
+        b_pos = n*ldb;
+        a_pos = n*ldc;
+        for (m = 0 ; m < M ; m++) {
+          c_pos = a_pos + m;
+          val = C[c_pos];
+          for (k = 0 ; k < K ; k++) {
+            val += alpha * A[k*lda + m] * B[b_pos + k] + beta;
+          } 
+          C[c_pos] = val;
+        }
+      }
+    }
+  }
+}
 
 long compute_ddot(long size, perf_t* start, perf_t* stop) {
   int i;
@@ -32,7 +129,6 @@ long compute_ddot(long size, perf_t* start, perf_t* stop) {
 }
 
 long compute_gemm(long size, perf_t* start, perf_t* stop, const enum CBLAS_TRANSPOSE trans) {
-  int i;
   double *A = d_matrix_create(size, size);
   double *B = d_matrix_create(size, size);
   double *C = d_matrix_create(size, size);
@@ -41,7 +137,7 @@ long compute_gemm(long size, perf_t* start, perf_t* stop, const enum CBLAS_TRANS
   d_matrix_fill(B, size, size, 5);
 
   perf(start);
-  cblas_dgemm(CblasColMajor, trans, CblasNoTrans, size, size, size, 1, A, size, B, size, 0, C, size);
+  cblas_dgemm_scalaire(CblasColMajor, trans, CblasNoTrans, size, size, size, 1, A, size, B, size, 0, C, size);
   perf(stop);
 
   d_matrix_free(A);
@@ -49,11 +145,36 @@ long compute_gemm(long size, perf_t* start, perf_t* stop, const enum CBLAS_TRANS
   d_matrix_free(C);
 
   if (trans == CblasNoTrans) {
-    return (size * size * (5*size + 3));
+    return (size * size * (5*size + 5));
   } else if (trans == CblasTrans) {
     return (size * size * (6*size + 1));
   } else {
-    return (size * (2 + size * (5 * size + 1)));
+    return (size * (2 + size * (7 * size + 1)));
+  }
+}
+
+long compute_gemm_block(long size, perf_t* start, perf_t* stop) {
+  double *A = d_matrix_create(size, size);
+  double *B = d_matrix_create(size, size);
+  double *C = d_matrix_create(size, size);
+
+  d_matrix_fill(A, size, size, 2);
+  d_matrix_fill(B, size, size, 5);
+  d_matrix_fill(C, size, size, 0);
+
+  perf(start);
+  cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, size, size, size, 1, A, size, B, size, 0, C, size);
+  perf(stop);
+
+  d_matrix_free(A);
+  d_matrix_free(B);
+  d_matrix_free(C);
+
+  int nb = ceil(1.0*size/100);
+  if (size <= 100) {
+    return size*(size*(size*6 + 1));
+  } else {
+    return (((nb*5 + 3)*nb+1)*nb + size*(size*(size*6+1)));
   }
 }
 
@@ -66,16 +187,43 @@ int main(int argc, char** argv) {
 
   while (size < MAX) { 
 
-    // DDOT
+    /* DDOT */
 
     /*flop = compute_ddot(size, &start, &stop);
-    perf_diff(&start, &stop);
-    performance = perf_mflops(&stop, flop);
+ *     perf_diff(&start, &stop);
+ *         performance = perf_mflops(&stop, flop);
+ *
+ *             printf("Size : %ld - Mflops/s : %lf\n", size, performance);
+ *                 fprintf(file, "%ld %lf\n", size, performance);
+ *                 */
+    /* GEMM */
 
-    printf("Size : %ld - Mflops/s : %lf\n", size, performance);
-    fprintf(file, "%ld %lf\n", size, performance);
-*/
-    // GEMM
+    /*flop = compute_gemm(size, &start, &stop, CblasNoTrans);
+ *     perf_diff(&start, &stop);
+ *         perf1 = perf_mflops(&stop, flop);
+ *
+ *             flop = compute_gemm(size, &start, &stop, CblasTrans);
+ *                 perf_diff(&start, &stop);
+ *                     perf2 = perf_mflops(&stop, flop);
+ *
+ *                         flop = compute_gemm(size, &start, &stop, CblasConjTrans);
+ *                             perf_diff(&start, &stop);
+ *                                 perf3 = perf_mflops(&stop, flop);
+ *
+ *                                     printf("Size : %ld - Mflop/s : %lf - %lf - %lf\n", size, perf1, perf2, perf3);
+ *                                         fprintf(file, "%ld %lf %lf %lf\n", size, perf1, perf2, perf3);
+ *                                             */
+
+    /* GEMM BLOCK */
+    
+    /*flop = compute_gemm_block(size, &start, &stop);
+ *     perf_diff(&start, &stop);
+ *         performance = perf_mflops(&stop, flop);
+ *
+ *             printf("Size : %ld - Mflop/s : %lf\n", size, performance);
+ *                 fprintf(file, "%ld %lf\n", size, performance);*/
+
+    /* GEMM VS GEMM BLOCK */
 
     flop = compute_gemm(size, &start, &stop, CblasNoTrans);
     perf_diff(&start, &stop);
@@ -89,12 +237,15 @@ int main(int argc, char** argv) {
     perf_diff(&start, &stop);
     perf3 = perf_mflops(&stop, flop);
 
-    printf("Size : %ld - Mflop/s : %lf - %lf - %lf\n", size, perf1, perf2, perf3);
-    fprintf(file, "%ld %lf %lf %lf\n", size, perf1, perf2, perf3);
+    flop = compute_gemm_block(size, &start, &stop);
+    perf_diff(&start, &stop);
+    performance = perf_mflops(&stop, flop);
+
+    printf("Size : %ld - Mflop/s : %lf - %lf - %lf - %lf\n", size, perf1, perf2, perf3, performance);
+    fprintf(file, "%ld %lf %lf %lf %lf\n", size, perf1, perf2, perf3, performance);
     
     size = size * COEF + ADD;
   }
   fclose(file);
   return 0;
 }
-
