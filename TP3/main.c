4541f537a91ae992df620a47c8b5b0b9e6695227
DevSquadMember <alexis.dufrenne@hotmail.fr>
2017-12-03 14:47:04 +0100

    TP3 - Fox ok

--- TP3/main.c
+++ TP3/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "mpi.h"
 #include "parser.h"
 #include "cblas.h"
-#include <string.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -100,12 +100,10 @@ int main(int argc, char **argv) {
     struct group world_group, grid_group, col_group, row_group;
     int nb_blocs;
     int dimensions[NB_DIMENSIONS], periods[NB_DIMENSIONS], remain_dims[NB_DIMENSIONS], grid_coords[NB_DIMENSIONS];
-    //int col_coords[1], row_coords[1];
     int reorder;
 
-    MPI_Status status;
-    MPI_Request request_recv;
-    MPI_Request request_send;
+    MPI_Status status_recv, status_send;
+    MPI_Request request_recv, request_send;
     MPI_Init(NULL, NULL);
 
     world_group.comm = MPI_COMM_WORLD;
@@ -174,9 +172,8 @@ int main(int argc, char **argv) {
 
     for (int i = 0 ; i < grid_group.size ; i++) {
         sendcounts[i] = 1;
-        displs[i] = (int) (floor(i / nb_blocs) * nb_blocs * local_size + i%nb_blocs);
-        //displs[i] = (int) (floor(i / local_size) * nb_blocs + i);
-        ///printf("displs %d is %d\n", i, displs[i]);
+        //displs[i] = (int) (floor(i / nb_blocs) * nb_blocs * local_size + i%nb_blocs);
+        displs[i] = (int) (i%nb_blocs) * nb_blocs * local_size + i/nb_blocs;
     }
 
     double* matrix_buffer_a = malloc(local_size*local_size* sizeof(double));
@@ -194,56 +191,56 @@ int main(int argc, char **argv) {
     MPI_Scatterv(matrix_a, sendcounts, displs, bloc, matrix_local_a, local_size*local_size, MPI_DOUBLE, 0, grid_group.comm);
     MPI_Scatterv(matrix_b, sendcounts, displs, bloc, matrix_local_b, local_size*local_size, MPI_DOUBLE, 0, grid_group.comm);
 
+    /*if (world_group.rank == 2) {
+        printf("Bloc B(%d, %d) \n", grid_coords[0], grid_coords[1]);
+        print_matrix(matrix_local_b, local_size);
+
+        printf("Bloc A(%d, %d) \n", grid_coords[0], grid_coords[1]);
+        print_matrix(matrix_local_a, local_size);
+    }*/
     /** Calcul **/
     for (int i = 0 ; i < local_size ; i++) {
         for (int j = 0 ; j < local_size ; j++) {
-            printf("Process %d has for A(%d, %d) : %lf\n", grid_group.rank, i, j, matrix_local_a[j*local_size + i]);
-            printf("Process %d has for B(%d, %d) : %lf\n", grid_group.rank, i, j, matrix_local_b[j*local_size + i]);
-            matrix_local_c[j*local_size + i] = grid_group.rank;
+            matrix_local_c[j*local_size + i] = 0;
         }
     }
     /** Fin des calculs **/
 
-    /** multiplication de fox **/
+    /** Multiplication de Fox **/
+
     for (int i = 0 ; i < nb_blocs ; i++ ) {
-        if(row_group.rank == (col_group.rank + i)%nb_blocs){
-	        memcpy(matrix_buffer_a, matrix_local_a, local_size*local_size*sizeof(double));
+
+        if (col_group.rank == (row_group.rank + i)%nb_blocs) {
+	        memcpy(matrix_buffer_a, matrix_local_a, local_size * local_size * sizeof(double));
         }
+
         // Broadcast de A sur la ligne
         MPI_Bcast(matrix_buffer_a, local_size * local_size, MPI_DOUBLE, (col_group.rank + i )%nb_blocs, row_group.comm);
+        //printf("Iteration %d - Bloc (%d, %d) received : %lf\n", i, grid_coords[0], grid_coords[1], matrix_buffer_a[0]);
+        ///printf("Iteration %d - Bloc (%d, %d) is receiving A from %d\n", i, grid_coords[0], grid_coords[1], (col_group.rank + i )%nb_blocs);
+
+        //printf("Iteration %d - Bloc A is for Bloc (%d, %d) \n", i, grid_coords[0], grid_coords[1]);
+        //print_matrix(matrix_buffer_a, local_size);
+
         // récupération de B
         if (i != 0) {
             MPI_Isend(matrix_local_b, local_size * local_size, MPI_DOUBLE, ((col_group.rank - 1) + nb_blocs) % nb_blocs, TAG, col_group.comm, &request_send);
             MPI_Irecv(matrix_local_b, local_size * local_size, MPI_DOUBLE, (col_group.rank + 1) % nb_blocs, TAG, col_group.comm, &request_recv);
 
+            MPI_Wait(&request_send, &status_send);
+            MPI_Wait(&request_recv, &status_recv);
+
+            //printf("Iteration %d - Bloc B is for Bloc (%d, %d) \n", i, grid_coords[0], grid_coords[1]);
+            //print_matrix(matrix_local_b, local_size);
+
             // multiplication des blocs
+            cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, local_size, local_size, local_size, 1., matrix_buffer_a, local_size, matrix_local_b, local_size, 1., matrix_local_c, local_size);
+        } else {
+            //printf("Iteration %d - Bloc B is for Bloc (%d, %d) \n", i, grid_coords[0], grid_coords[1]);
+            //print_matrix(matrix_local_b, local_size);
             cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, local_size, local_size, local_size, 1., matrix_buffer_a, local_size, matrix_local_b, local_size, 1., matrix_local_c, local_size);
         }
     }
-
-
-    /**if (col_rank == 1) {
-        W = rank;
-        V[0] = rank;
-        V[1] = rank;
-        V[2] = rank;
-    }
-
-    // Envoi de V aux voisins avec le rang de la deuxième cellule
-    //MPI_Scatter(V, 1, MPI_INT, &W, 1, MPI_INT, 1, column);
-
-    // Communication avec les voisins (dessus et dessous)
-    if (col_rank == 1) {
-        int rank_top, rank_bottom;
-        MPI_Cart_shift(column, 0, 1, &rank_bottom, &rank_top);
-        MPI_Send(&W, 1, MPI_INT, rank_bottom, 99, column);
-        MPI_Send(&W, 1, MPI_INT, rank_top, 99, column);
-    } else {
-        MPI_Recv(&W, 1, MPI_INT, 1, 99, column, &status);
-    }
-
-    printf("Proc %d is %d/%d (%d;%d) in grid and %d/%d (%d) in col - W : %d\n", rank, grid_rank, grid_size-1, grid_coords[0], grid_coords[1], col_rank, col_size-1, col_coords[0], W);
-**/
 
     MPI_Gatherv(matrix_local_c, local_size*local_size, MPI_DOUBLE, matrix_c, sendcounts, displs, bloc, 0, grid_group.comm);
 
