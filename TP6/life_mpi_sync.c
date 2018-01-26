bc20f2865c1f411d10ab259fba41da27e7d1e826
DevSquadMember <alexis.dufrenne@hotmail.fr>
2018-01-26 02:18:07 +0100

    TP6 - pthread

--- TP6/life_mpi_sync.c
+++ TP6/life_mpi_sync.c
@@ -138,16 +138,16 @@ int main(int argc, char* argv[]) {
     for (i = 0 ; i < grid_group.size ; i++) {
         sendcounts[i] = 1;
         displs[i] = i % nb_blocs * nb_blocs * (local_ldboard - 1) + i / nb_blocs;
-        displs2[i] = i % nb_blocs * nb_blocs * (local_ldboard - 2) + i / nb_blocs;
+        displs2[i] = i % nb_blocs * nb_blocs * (local_ldboard - 2) * (local_ldboard - 2) + local_ldboard * i / nb_blocs;
 
-        //printf("displs[%d] = %d\n", i, displs[i]);
+        printf("displs[%d] = %d\n", i, displs[i]);
     }
 
     // Définition du bloc pour chaque processeur
     MPI_Datatype bloc;
     MPI_Type_vector(local_ldboard - 1, local_ldboard, ldboard, MPI_INT, &bloc);
     //MPI_Type_create_resized(bloc, 0, (local_ldboard - 2) * sizeof(int), &bloc);
-    MPI_Type_create_resized(bloc, 0, (local_ldboard) * sizeof(int), &bloc);
+    MPI_Type_create_resized(bloc, 0, (1) * sizeof(int), &bloc);
     MPI_Type_commit(&bloc);
 
     MPI_Datatype row;
